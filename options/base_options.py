2d6f94d304d3d2bc7e8928ba96131f32c336803e
sonsus <sison@bi.snu.ac.kr>
2018-02-07 14:18:13 +0900

    baseoption: expname default chgd, trainoption: savefreq chgd

--- options/base_options.py
+++ options/base_options.py
@@ -22,7 +22,6 @@ class BaseOptions():
         self.parser.add_argument('--which_model_netG', type=str, default='resnet_9blocks', help='selects model to use for netG')
         self.parser.add_argument('--n_layers_D', type=int, default=3, help='only used if which_model_netD==n_layers')
         self.parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
-        self.parser.add_argument('--name', type=str, default='experiment_name', help='name of the experiment. It decides where to store samples and models')
         self.parser.add_argument('--dataset_mode', type=str, default='aligned', help='chooses how datasets are loaded. [unaligned | aligned | single]')
         self.parser.add_argument('--model', type=str, default='cycle_gan',
                                  help='chooses which model to use. cycle_gan, pix2pix, test')
@@ -39,6 +38,8 @@ class BaseOptions():
         self.parser.add_argument('--resize_or_crop', type=str, default='resize_and_crop', help='scaling and cropping of images at load time [resize_and_crop|crop|scale_width|scale_width_and_crop]')
         self.parser.add_argument('--no_flip', action='store_true', help='if specified, do not flip the images for data augmentation')
         self.parser.add_argument('--init_type', type=str, default='xavier', help='network initialization [normal|xavier|kaiming|orthogonal]')
+        self.parser.add_argument('--name', type=str, default='G_{g}_D_{d}_{data}'.format(g=args.which_model_netG, d=args.which_model_netD, data=args.dataroot.split('/')[-1]), 
+                                help='name of the experiment. It decides where to store samples and models')
 
         self.initialized = True
 
