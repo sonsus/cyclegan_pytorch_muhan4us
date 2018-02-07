e2972f700f73c8f212e8ff5a69747ea4487f6b69
sonsus <sison@bi.snu.ac.kr>
2018-02-07 14:24:52 +0900

    args1 fix

--- options/base_options.py
+++ options/base_options.py
@@ -10,7 +10,6 @@ class BaseOptions():
         self.initialized = False
 
     def initialize(self):
-        args1=argparse.ArgumentParser(description="").parse_args()
         self.parser.add_argument('--dataroot', default="./datasets/bolbbalgan4", help='path to images (should have subfolders trainA, trainB, valA, valB, etc)')
         self.parser.add_argument('--batchSize', type=int, default=1, help='input batch size')
         self.parser.add_argument('--loadSize', type=int, default=1, help='scale images to this size')
@@ -39,6 +38,7 @@ class BaseOptions():
         self.parser.add_argument('--resize_or_crop', type=str, default='resize_and_crop', help='scaling and cropping of images at load time [resize_and_crop|crop|scale_width|scale_width_and_crop]')
         self.parser.add_argument('--no_flip', action='store_true', help='if specified, do not flip the images for data augmentation')
         self.parser.add_argument('--init_type', type=str, default='xavier', help='network initialization [normal|xavier|kaiming|orthogonal]')
+        args1=argparse.ArgumentParser(description="").parse_args()
         self.parser.add_argument('--name', type=str, default='G_{g}_D_{d}_{data}'.format(g=args1.which_model_netG, d=args1.which_model_netD, data=args1.dataroot.split('/')[-1]), 
                                 help='name of the experiment. It decides where to store samples and models')
 
